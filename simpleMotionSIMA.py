7bd1e2a17a28e62324ccacb82f27cd9a5cecb581
darkmajere <gokhanordk@gmail.com>
2016-10-26 15:43:21 -0400

    testData is included for WorkShop

--- simpleMotionSIMA.py
+++ simpleMotionSIMA.py
@@ -10,8 +10,12 @@ def motion(datadir):
 	sequences = [sima.Sequence.create('TIFF', datadir)]
 	mc_approach = sima.motion.PlaneTranslation2D(max_displacement=[15, 30])
 	dataset = mc_approach.correct(sequences, 'example_translation2D.sima')
+	mc_approach2 = sima.motion.HiddenMarkov2D(granularity='row', max_displacement=[80, 100], verbose=False)
+	dataset2=mc_approach2.correct(sequences, 'example_HM.sima')
 	dataset.export_frames([[['frames.tif']]], fmt='TIFF16')
-	
+	#dataset2.export_frames([['frames_HM2D.tif']],fmt='TIFF16')
+	dataset.export_averages(['avgs.tif'],fmt='TIFF16')
+	dataset2.export_averages(['avgsMC.tif'],fmt='TIFF16')
 
 
 
